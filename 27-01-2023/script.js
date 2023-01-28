82937caabce4459adae9478a41f064519f45a7d1
SoniaGrassia <grassiasonia@gmail.com>
2023-01-28 16:55:39 +0100

    Esercizi 27 Gennaio 2023

--- 27-01-2023/script.js
+++ 27-01-2023/script.js
@@ -2,11 +2,19 @@ import { GET1 } from "./api.js";
 
 import { qS, cardPopulator } from "./utils.js";
 
-const popularFilms = qS(".popular");
-const topRatedFilms = qS(".top-rated");
+const popularFilms = qS(".popular-movies");
+const topRatedFilms = qS(".top-rated-movies");
+const popularTv = qS(".popular-tv");
+const topRatedTv = qS(".top-rated-tv");
 
-Promise.all([GET1("tv", "popular"), GET1("tv", "top_rated")]).then((data) => {
+Promise.all([
+  GET1("movie", "popular"),
+  GET1("movie", "top_rated"),
+  GET1("tv", "popular"),
+  GET1("tv", "top_rated"),
+]).then((data) => {
   data[0].results.map((movie) => popularFilms.append(cardPopulator(movie)));
-
   data[1].results.map((movie) => topRatedFilms.append(cardPopulator(movie)));
+  data[2].results.map((movie) => popularTv.append(cardPopulator(movie)));
+  data[3].results.map((movie) => topRatedTv.append(cardPopulator(movie)));
 });
