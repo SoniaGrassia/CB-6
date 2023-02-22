a98436b5db79e614c5ff236f382df6daa9d80818
SoniaGrassia <grassiasonia@gmail.com>
2023-02-22 18:03:07 +0100

    Code Week

--- Code_Week/loginScript.js
+++ Code_Week/loginScript.js
@@ -1,1 +1,39 @@
+import { qS } from "./utils.js";
+const userName = qS("input[name='username']");
+const userPassword = qS("input[name='password']");
+const loginForm = qS(".loginForm");
+const errorMsg = qS(".modal-error");
 
+const myLogin = {
+  username: "sonia",
+  password: "1111",
+};
+
+window.onload = init();
+
+function init() {
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    userLogin();
+  });
+}
+
+function userLogin() {
+  let nameVal = userName.value,
+    passwordVal = userPassword.value;
+
+  if (nameVal === myLogin.username && passwordVal === myLogin.password) {
+    loginForm.remove();
+    //devi creare MyList
+  } else {
+    errorMsg.classList.add("enabled");
+    loginForm.append(errorMsg);
+
+    setTimeout(function () {
+      errorMsg.classList.remove("enabled");
+      loginForm.reset();
+    }, 2000);
+  }
+}
+
+// non funziona
